fa77d10951830f9ec0f7ea2a24642068642a9f71
Apoorv Pachori <apoorv.pachori21@gmail.com>
2021-03-25 20:33:20 -0700

    trying vscode source control

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ $servername = "practicedatabase.czxgxu5qixl3.us-west-1.rds.amazonaws.com";
 $username = "admin";
 $password = "adarshhi1poofy";
 $databasename = "example";
-
-
 $conn = mysqli_connect($servername,$username,$password,$databasename);
-
 ?>
 
 <!DOCTYPE html>
